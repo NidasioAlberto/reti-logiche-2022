293b6e8742d0752960409e2f82d521b958c9f192
Alberto Nidasio <alberto.nidasio@skywarder.eu>
2022-05-10 12:16:50 +0200

    Implemented U bytes reading

--- project/project.srcs/esempio_1/new/tb_esempio_1.vhd
+++ project/project.srcs/esempio_1/new/tb_esempio_1.vhd
@@ -1,4 +1,3 @@
-
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -8,7 +7,7 @@ entity project_tb is
 end project_tb;
 
 architecture projecttb of project_tb is
-    constant c_CLOCK_PEZRIOD       : time := 100 ns;
+    constant c_CLOCK_PERIOD       : time := 15 ns;
     signal tb_done                : std_logic;
     signal mem_address            : std_logic_vector (15 downto 0) := (others => '0');
     signal tb_rst                 : std_logic                      := '0';
@@ -17,6 +16,11 @@ architecture projecttb of project_tb is
     signal mem_o_data, mem_i_data : std_logic_vector (7 downto 0);
     signal enable_wire            : std_logic;
     signal mem_we                 : std_logic;
+    --signal   test_read              : std_logic;
+    --signal   test_process           : integer;
+    --signal   test_write             : integer;
+    --signal   test_address           : std_logic_vector (15 downto 0);
+    --signal   test_end_address       : std_logic_vector (15 downto 0);
 
     type ram_type is array (65535 downto 0) of std_logic_vector(7 downto 0);
     signal RAM : ram_type := (0 => std_logic_vector(to_unsigned(2, 8)),
@@ -24,35 +28,45 @@ architecture projecttb of project_tb is
     2                           => std_logic_vector(to_unsigned(75, 8)),
     others => (others => '0'));
     -- Expected Output  1000 -> 209                         
-    -- Expected Output  1001 -> 205                         
-    -- Expected Output  1002 -> 247                         
-    -- Expected Output  1003 -> 201                         
+    -- Expected Output  1001  -> 205                         
+    -- Expected Output  1002  -> 247                         
+    -- Expected Output  1003  -> 210                         
 
     component project_reti_logiche is
         port (
             i_clk     : in std_logic;
-            i_start   : in std_logic;
             i_rst     : in std_logic;
+            i_start   : in std_logic;
             i_data    : in std_logic_vector(7 downto 0);
             o_address : out std_logic_vector(15 downto 0);
             o_done    : out std_logic;
             o_en      : out std_logic;
             o_we      : out std_logic;
             o_data    : out std_logic_vector (7 downto 0)
+            --      t_read        : out std_logic;
+            --      t_process     : out integer;
+            --      t_write       : out integer;
+            --      t_address     : out std_logic_vector(15 downto 0);
+            --      t_end_addr    : out std_logic_vector(15 downto 0) 
         );
     end component project_reti_logiche;
 begin
     UUT : project_reti_logiche
     port map(
         i_clk     => tb_clk,
-        i_start   => tb_start,
         i_rst     => tb_rst,
+        i_start   => tb_start,
         i_data    => mem_o_data,
         o_address => mem_address,
         o_done    => tb_done,
         o_en      => enable_wire,
         o_we      => mem_we,
         o_data    => mem_i_data
+        --         t_read        => test_read,
+        --          t_process     => test_process,
+        --          t_write       => test_write,
+        --          t_address     => test_address,
+        --          t_end_addr    => test_end_address
     );
 
     p_CLK_GEN : process is
@@ -66,16 +80,15 @@ begin
             if enable_wire = '1' then
                 if mem_we = '1' then
                     RAM(conv_integer(mem_address)) <= mem_i_data;
-                    mem_o_data                     <= mem_i_data after 1 ns;
+                    mem_o_data                     <= mem_i_data after 2 ns;
                 else
-                    mem_o_data <= RAM(conv_integer(mem_address)) after 1 ns;
+                    mem_o_data <= RAM(conv_integer(mem_address)) after 2 ns;
                 end if;
             end if;
         end if;
     end process;
     test : process is
     begin
-        tb_start <= '0';
         wait for 100 ns;
         wait for c_CLOCK_PERIOD;
         tb_rst <= '1';
@@ -92,16 +105,14 @@ begin
         wait until tb_done = '0';
         wait for 100 ns;
 
-        -- Input Stream =  [162, 75]  
-        -- Output Stream =  [209, 205, 247, 210]  
+        -- Input=  [162, 75]  
+        -- Output =  [209, 205, 247, 210]  
 
-        assert RAM(1000) = std_logic_vector(to_unsigned(209, 8)) report "TEST FALLITO (ENCODE). Expected  209  found " & integer'image(to_integer(unsigned(RAM(1000)))) severity failure;
-        assert RAM(1001) = std_logic_vector(to_unsigned(205, 8)) report "TEST FALLITO (ENCODE). Expected  205  found " & integer'image(to_integer(unsigned(RAM(1001)))) severity failure;
-        assert RAM(1002) = std_logic_vector(to_unsigned(247, 8)) report "TEST FALLITO (ENCODE). Expected  247  found " & integer'image(to_integer(unsigned(RAM(1002)))) severity failure;
-        assert RAM(1003) = std_logic_vector(to_unsigned(210, 8)) report "TEST FALLITO (ENCODE). Expected  210  found " & integer'image(to_integer(unsigned(RAM(1003)))) severity failure;
-        assert RAM(1004) = std_logic_vector(to_unsigned(0, 8)) report "TEST FALLITO (POST_ENCODE). Expected  0  found " & integer'image(to_integer(unsigned(RAM(1004)))) severity failure;
-
-        assert false report "Simulation Ended! TEST PASSATO (ENCODE_EXAMPLE)" severity failure;
+        assert RAM(1000) = std_logic_vector(to_unsigned(209, 8)) report "TEST FALLITO (WORKING ZONE). Expected  209  found " & integer'image(to_integer(unsigned(RAM(1000)))) severity failure;
+        assert RAM(1001) = std_logic_vector(to_unsigned(205, 8)) report "TEST FALLITO (WORKING ZONE). Expected  205  found " & integer'image(to_integer(unsigned(RAM(1001)))) severity failure;
+        assert RAM(1002) = std_logic_vector(to_unsigned(247, 8)) report "TEST FALLITO (WORKING ZONE). Expected  247  found " & integer'image(to_integer(unsigned(RAM(1002)))) severity failure;
+        assert RAM(1003) = std_logic_vector(to_unsigned(210, 8)) report "TEST FALLITO (WORKING ZONE). Expected  210  found " & integer'image(to_integer(unsigned(RAM(1003)))) severity failure;
+        assert false report "Simulation Ended! TEST PASSATO" severity failure;
     end process test;
 
 end projecttb;
--- project/project.srcs/sources_1/new/project_reti_logiche.vhd
+++ project/project.srcs/sources_1/new/project_reti_logiche.vhd
@@ -7,6 +7,8 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
+use IEEE.STD_LOGIC_UNSIGNED.all;
+use IEEE.NUMERIC_STD.all;
 
 entity project_reti_logiche is
     port (
@@ -23,10 +25,87 @@ entity project_reti_logiche is
 end project_reti_logiche;
 
 architecture priject_arch of project_reti_logiche is
+    type state_type is (IDLE, REQUEST_W, FETCH_W, REQUEST_U, COMPUTE_U, WRITE_P1, WRITE_P2, DONE);
+    signal current_state   : state_type             := IDLE;
+    signal next_state      : state_type             := IDLE;
+    signal W               : integer range 0 to 255 := 0;
+    signal current_U_count : integer range 0 to 255 := 0;
+    signal current_U       : std_logic_vector(7 downto 0);
 begin
-    o_address <= "0000000000000000";
-    o_done <= '0';
-    o_en <= '0';
-    o_we <= '0';
-    o_data <= "00000000";
+    -- Handle reset and clock inputs and updates the state
+    process (i_rst, i_clk)
+    begin
+        if (i_rst = '1') then
+            -- Reset the state whenever the reset signal is up
+            current_U     <= "00000000";
+            current_state <= IDLE;
+        elsif falling_edge(i_clk) then
+            -- Advance to the next state at the clock's rising edge
+            current_state <= next_state;
+        end if;
+    end process;
+
+    -- Finite state machine
+    process (current_state, i_start)
+    begin
+        -- Default outputs
+        o_address <= "0000000000000000";
+        o_done    <= '0';
+        o_en      <= '0';
+        o_we      <= '0';
+        o_data    <= "00000000";
+
+        case current_state is
+            when IDLE =>
+                if (i_start = '1') then
+                    next_state <= REQUEST_W;
+                else
+                    W               <= 0;
+                    current_U_count <= 0;
+                    next_state      <= IDLE;
+                end if;
+            when REQUEST_W =>
+                -- Request the first address in memory where W is located
+                o_address <= std_logic_vector(to_unsigned(0, o_address'length));
+                o_en      <= '1';
+
+                next_state <= FETCH_W;
+            when FETCH_W =>
+                -- Read W from the memory data bus
+                W <= CONV_INTEGER(i_data);
+
+                next_state <= REQUEST_U;
+            when REQUEST_U =>
+                -- Request the current U byte address
+                o_address <= std_logic_vector(to_unsigned(current_U_count + 1, o_address'length));
+                o_en      <= '1';
+
+                next_state <= COMPUTE_U;
+            when COMPUTE_U =>
+                -- Read the current U byte from the memory data bus
+                current_U <= i_data;
+
+                -- Run the 1/2 convolutional code
+                -- TODO
+
+                -- TODO: Move this to WRITE_P2
+                if (current_U_count = W) then
+                    -- If all U bytes have been read stop here
+                    o_done     <= '1';
+                    next_state <= DONE;
+                else
+                    -- Otherwise update the current_U_count and continue
+                    current_U_count <= current_U_count + 1;
+                    next_state      <= REQUEST_U;
+                end if;
+            when WRITE_P1 =>
+            when WRITE_P2 =>
+            when DONE     =>
+                if (i_start = '0') then
+                    -- If the start signal goes back to 0, reset the done signal
+                    o_done <= '0';
+                end if;
+        end case;
+    end process;
+
 end priject_arch;
